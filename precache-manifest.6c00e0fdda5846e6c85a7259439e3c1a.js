50ac01ea0c2b8212c874e671002f3e53e610185d
=AjRajan7 <newacct7robot@gmail.com>
2020-07-06 15:08:26 +0530

    Updates

---
+++ precache-manifest.6c00e0fdda5846e6c85a7259439e3c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0c4f600d55e584b660d2bf5a4856f7",
+    "url": "/suma/index.html"
+  },
+  {
+    "revision": "874a22ff180de1f14af6",
+    "url": "/suma/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c7df8478e0aeaae8592",
+    "url": "/suma/static/js/2.e7374bc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/suma/static/js/2.e7374bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874a22ff180de1f14af6",
+    "url": "/suma/static/js/main.83505446.chunk.js"
+  },
+  {
+    "revision": "c0cb99f0d5d237dae9ef",
+    "url": "/suma/static/js/runtime-main.8c216744.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/suma/static/media/logo.5d5d9eef.svg"
+  }
+]);
